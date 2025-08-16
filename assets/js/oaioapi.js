8490d277db0c24fc52062e7e13ad26c0916472e6
alxfed <alxfed@gmail.com>
2025-08-16 12:42:48 -0500

    initial commit

--- assets/js/oaioapi.js
+++ assets/js/oaioapi.js
@@ -17,8 +17,8 @@ self.onmessage = async function (event) {
 		// --- 2. Fetch instruction ---
 		let instructionText; // Declare here to ensure it's in scope
 		try {
-			console.log('Worker: Fetching the Machine instruction from https://localhost');
-			const instructionResponse = await fetch('https://localhost/' + machineConfig.instructions_file);
+			console.log('Worker: Fetching the Machine instruction from https://localhost:8443');
+			const instructionResponse = await fetch('https://localhost:8443/' + machineConfig.instructions_file);
 			if (!instructionResponse.ok) {
 				console.log(`Worker: HTTP error fetching instruction! status: ${instructionResponse.status}. Using default instruction.`);
 				// Default instruction if fetching fails or file not found
